e16baa5d23e8250442e005eb21cf184a6e977f08
dpvasani <vadanidarshan@gmail.com>
2024-12-20 21:52:52 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -5,35 +5,37 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+// Routes
 const bookingRoute = require("./Routes/movieRoute");
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Configure CORS to accept requests from any origin
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" })); // Allow requests from any origin
 
 // Determine the base URL based on the environment mode
-const baseURL = process.env.MODE === "development" 
-  ? "http://localhost:3001" 
-  : "http://backendonrender.com";
+const baseURL =
+  process.env.MODE === "development"
+    ? "http://localhost:3001"
+    : "http://backendonrender.com";
 
 console.log(`Base URL set to: ${baseURL}`);
 
-// Connecting to database
+// Connect to the database
 connection();
 
 // Home route
-app.get("/", async (req, res) => {
-    res.send("Hi, I am the home page");
+app.get("/", (req, res) => {
+  res.send("Hi, I am the home page");
 });
 
-// Use booking routes with the base URL as context
+// Booking routes
 app.use("/api", bookingRoute);
 
-// Listening on the specified port
+// Start the server
 const port = process.env.PORT || 8081;
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
 
 module.exports = app;
